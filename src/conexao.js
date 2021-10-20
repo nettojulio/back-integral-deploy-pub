52a6e336ae4a5c194e9fb53a7626809b39749bef
Julio Neto <nettojulio@hotmail.com>
2021-10-20 17:06:15 -0300

    Config da conexão com o DB/Heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,14 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
+        host: 'ec2-3-213-41-172.compute-1.amazonaws.com',
         port: 5432,
-        user: 'postgres',
-        password: '123456',
-        database: 'market_cubos'
+        user: 'aqpinujrpiucab',
+        password: '24bec2944339018eda841a1e32f0b76d3902eaaacbe98f56dd37236e571ac7cc',
+        database: 'dai7c6ul5dgdti',
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
